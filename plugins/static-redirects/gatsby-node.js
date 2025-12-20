cf3420731993fd837ce86665ff44308b6d86892c
nmoskaleva <natalia.moskaleva@criipto.com>
2025-12-20 14:38:59 +0100

    Fix missing _redirects file during build
    Build was failing on this branch with ENOENT when reading `public/_redirects`. Fixed by initializing `input` to empty string if `_redirects` doesn't exist.

--- plugins/static-redirects/gatsby-node.js
+++ plugins/static-redirects/gatsby-node.js
@@ -6,7 +6,10 @@ const redirects = [
 
 exports.onPostBuild = ({ reporter, basePath, pathPrefix }) => {
   const redirectsPath = path.resolve(`${__dirname}/../../public/_redirects`);
-  const input = fs.readFileSync(redirectsPath, 'utf-8');
+  let input = "";
+  if (fs.existsSync(redirectsPath)) {
+    input = fs.readFileSync(redirectsPath, "utf-8");
+  }
   const output = `${input}\n${redirects.join('\n')}`;
   console.log(output);
   fs.writeFileSync(redirectsPath, output);
